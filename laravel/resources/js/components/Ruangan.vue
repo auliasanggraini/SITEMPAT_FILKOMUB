f0daed7231bac38bb3c3b758ad71e6e99e9b8138
auliasanggraini <112447425+auliasanggraini@users.noreply.github.com>
2024-05-16 14:58:11 +0700

    up

---
+++ laravel/resources/js/components/Ruangan.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Daftar Ruangan</h2>
+        <ul>
+            <li v-for="ruangan in ruangans" :key="ruangan.id">
+                {{ ruangan.nama }} - Kapasitas: {{ ruangan.kapasitas }} - Fasilitas: {{ ruangan.fasilitas }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ruangans: []
+        };
+    },
+    mounted() {
+        axios.get('/api/ruangans')
+            .then(response => {
+                this.ruangans = response.data;
+            });
+    }
+}
+</script>
